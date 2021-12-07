8f4f02cb8b977c6063246911d093ece1c5758798
lindacolumbus <lindadcolumbus@gmail.com>
2021-12-07 16:17:58 -0500

    tweaked styling; connected script file

---
+++ script.js
@@ -0,0 +1,22 @@
+// const portfolio = {};
+
+// portfolio.startEventListener = () => {
+//     // Next arrow click event to cycle through projects on home page
+//     const nextButton = document.getElementsByClassName('next');
+//     nextButton[0].addEventListener('click', function(event) {
+//         console.log('clicked');
+//         const slideshowImage1 = document.getElementsByClassName('projectSlideshow1');
+//         const slideshowImage2 = document.getElementsByClassName('projectSlideshow2');
+//         slideshowImage1[0].src = './assets/Weather Scout1.png';
+//         slideshowImage2[0].src = './assets/Weather Scout2.png';
+//     })
+// }
+
+
+// portfolio.init = () => {
+//     // Listen for Form Submission
+//     portfolio.startEventListener()
+//     // cleanUrl()
+// }
+
+// portfolio.init();
