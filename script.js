d926e539fc54f51ad3b7c7e49ef7cebf48a1523c
lindacolumbus <lindadcolumbus@gmail.com>
2021-12-08 22:07:19 -0500

    working on media queries for about page

--- script.js
+++ script.js
@@ -15,7 +15,9 @@ portfolio.projects = [
     tasteMaker = {
     projectNum: '01 / 03',
     srcImage1: './assets/Taste Maker1.png',
+    alt1: 'Pink headphones on a pink and teal backdrop" class="projectSlideshow1',
     srcImage2: './assets/Taste Maker2.png',
+    alt2: 'Music recommendation of BORNS based off of a Glass Animals musician search query',
     skillsHTML: '<ul><li><p class="tagline">HTML5</p></li><li><p class="tagline">CSS</p></li><li><p class="tagline">JavaScript</p></li><li><p class="tagline">API</p></li><li><p class="tagline">React</p></li><li><p class="tagline">Firebase</p></li></ul>',
     title: 'Taste Maker'
     },
@@ -23,7 +25,9 @@ portfolio.projects = [
     weatherScout = {
     projectNum: '02 / 03',
     srcImage1: './assets/Weather Scout1.png',
+    alt1: 'Pink and cream coloured clouds on a purple sky',
     srcImage2: './assets/Weather Scout2.png',
+    alt2: 'Screenshot of form prompting user for a city name for a weather forecast',
     skillsHTML: '<ul><li><p class="tagline">HTML5</p></li><li><p class="tagline">CSS</p></li><li><p class="tagline">SCSS</p></li><li><p class="tagline">JavaScript</p></li><li><p class="tagline">API</p></li></ul>',
     title: 'Weather Scout'
     },
@@ -31,7 +35,9 @@ portfolio.projects = [
     delicious = {
         projectNum: '03 / 03',
         srcImage1: './assets/Delicious 1.jpeg',
+        alt1: 'Top down view of two pink coloured drinks, garnished with blueberries and a lime slice',
         srcImage2: './assets/Delicious2.png',
+        alt2: 'Screenshot of a blog page with related posts and comments',
         skillsHTML: '<ul class="shortSkillList"><li><p class="tagline">HTML5</p></li><li><p class="tagline">CSS</p></li><li><p class="tagline">SCSS</p></li></ul>',
         title: 'Delicious'
     }
@@ -47,7 +53,9 @@ portfolio.startEventListener = () => {
     const changeImage = (value) => {
         portfolio.projectNum[0].textContent = `${portfolio.projects[value].projectNum}`;
         portfolio.slideshowImage1[0].src = `${portfolio.projects[value].srcImage1}`;
+        portfolio.slideshowImage1[0].alt = `${portfolio.projects[value].alt1}`;
         portfolio.slideshowImage2[0].src = `${portfolio.projects[value].srcImage2}`;
+        portfolio.slideshowImage2[0].alt = `${portfolio.projects[value].alt2}`;
         portfolio.skillsList[0].innerHTML = `${portfolio.projects[value].skillsHTML}`
         portfolio.projectTitle[0].textContent = `${portfolio.projects[value].title}`
     }
